34ed25b988011bf8d265b3ad7cc94f894f2731cc
Ermek <tolymbekov_ermek@mail.ru>
2023-10-07 14:24:43 +0600

    Added basic routing

---
+++ src/pages/Game.jsx
@@ -0,0 +1,31 @@
+import { Box, Typography } from "@mui/material";
+
+function Game() {
+  
+
+  /**
+   * Layout
+   * Game title
+   * screens / trailer
+   * description
+   * tags
+   * rating
+   * 
+   * price
+   * add to wishlist
+   * 
+   * specs
+   * 
+   * reviews
+   * 
+   */
+  return (
+    <Box>
+      <Typography variant="h1" >
+        Game title
+      </Typography>
+    </Box>
+  )
+}
+
+export default Game;
