a0d41de6e6e94b1dbb8d1c1e0bda11477ab95a68
Ermek <tolymbekov_ermek@mail.ru>
2023-10-07 16:04:58 +0600

    Updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,40 @@
-import { AppBar, Avatar, Box, Toolbar } from "@mui/material";
+import { Notifications } from "@mui/icons-material";
+import { AppBar, Avatar, Badge, Box, Stack, Toolbar, Typography } from "@mui/material";
 import { Link } from 'react-router-dom';
-// import { AvatarIcon } from '../../static/';
+// import { Ava tarIcon } from '../../static/';
 
 function Navbar() {
   return (
     <AppBar position='sticky'>
       <Toolbar sx={{
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "space-around",
         gap: '20px',
       }}>
-        <Link to="/" >Store</Link>
-        <Link to="/library" >Library</Link>
-
         <Box>
+
+        </Box>
+        <Stack direction='row' gap={20}>
+          <Typography sx={{fontSize: '16px'}}>
+            <Link to="/" >Store</Link>
+          </Typography>
+          <Typography sx={{fontSize: '16px'}}>
+            <Link to="/library" >Library</Link>
+          </Typography>
+          <Typography sx={{fontSize: '16px'}}>
+            <Link to="/downloads">Downloads</Link>
+          </Typography>
+        </Stack>
+
+        <Stack direction='row' gap={5} alignItems='center'>
+          <Badge badgeContent={2} color="success">
+            <Notifications />
+          </Badge>
           <Avatar 
-            sx={{ width: 30, height: 30}}
+            sx={{ width: 40, height: 40}}
             // src={AvatarIcon}
            />
-        </Box>
+        </Stack>
       </Toolbar>
     </AppBar>
   );
